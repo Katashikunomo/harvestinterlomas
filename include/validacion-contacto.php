cde0d96412aa0c202d91d50ae17d0141f825ac82
Alexis Malfavaun <2203024384@alumnos.cua.uam.mx>
2024-02-22 07:25:38 -0600

    commit

--- include/validacion-contacto.php
+++ include/validacion-contacto.php
@@ -21,7 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mensaje .= "\n\nDatos de inscripción:\nNombre del tutor: $nombreTutor\nRelación: $relacion\nNivel Académico: $nivelAcademico\nGénero: $genero\nEdad: $edad años";
     }
 
-    $sql = "INSERT INTO registro_correos_h (dt_nombre,dt_numero,dt_email,dt_nombre_t,dt_relacion,dt_nivel,dt_genero,dt_edad,dt_mensaje,dt_fecha,dt_asunto) values(UPPER('{$nombre}'),'{$numero}',UPPER('{$email}'), UPPER('{$nombreTutor}'),'{$relacion}','{$nivelAcademico}','{$genero}','{$edad}','{$mensaje}',CURDATE(),'{$asunto}')";
+    $sql = "INSERT INTO registro_correos_h (dt_nombre,dt_numero,dt_email,dt_nombre_t,dt_relacion,dt_nivel,dt_genero,dt_edad,dt_mensaje,dt_asunto) values(UPPER('{$nombre}'),'{$numero}',UPPER('{$email}'), UPPER('{$nombreTutor}'),'{$relacion}','{$nivelAcademico}','{$genero}','{$edad}','{$mensaje}','{$asunto}')";
     $mysqli->query($sql);
     // Configurar el destinatario y el asunto del correo
     $destinatario = "admisiones@harvestinterlomas.com, malfatapia1@gmail.com";
